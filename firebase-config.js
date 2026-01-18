913ae6e5320ad10bd970690daf41a1ad796c365d
emperor6007 <68384816+emperor6007@users.noreply.github.com>
2026-01-18 04:54:34 +0100

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAoy0fqrDdbjTkF1cnFSXH0UnPgVRugfJs",
+  authDomain: "authenticator-ac9b1.firebaseapp.com",
+  projectId: "authenticator-ac9b1",
+  storageBucket: "authenticator-ac9b1.firebasestorage.app",
+  messagingSenderId: "219790206178",
+  appId: "1:219790206178:web:cd208e5642de61ec216abb"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+
+// Export for use in other files
+window.db = database;
